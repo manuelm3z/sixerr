35a9b9041adf7d8d548d8a820a884ae58b6b97e9
Manuel Zambrano <mmzmm3z@gmail.com>
2016-12-09 20:06:53 -0300

    Update base templates

--- sixerr/settings.py
+++ sixerr/settings.py
@@ -139,7 +139,24 @@ LOGIN_REDIRECT_URL = '/'
 SOCIAL_AUTH_FACEBOOK_KEY = '398219613902468'
 SOCIAL_AUTH_FACEBOOK_SECRET = '52b288e5d8bc69004234219873f42d13'
 
+SOCIAL_AUTH_PIPELINE = (
+    'social.pipeline.social_auth.social_details',
+    'social.pipeline.social_auth.social_uid',
+    'social.pipeline.social_auth.auth_allowed',
+    'social.pipeline.social_auth.social_user',
+    'social.pipeline.user.get_username',
+    'social.pipeline.user.create_user',
+    'sixerrapp.social_auth_pipeline.save_avatar',  # <--- set the path to the function
+    'social.pipeline.social_auth.associate_user',
+    'social.pipeline.social_auth.load_extra_data',
+    'social.pipeline.user.user_details',
+)
+
 # Replace database settings to use postgresql on Heroku
 import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
+
+# Setup upload directory for Gig model
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
