dfc50dfed1ddd1bafea13cdcf75446cf7914e345
Manuel Zambrano <mmzmm3z@gmail.com>
2016-12-09 23:16:24 -0300

    Update Create-Gig page

--- sixerrapp/views.py
+++ sixerrapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Gig
+from .forms import GigForm
 
 # Create your views here.
 def home(request):
@@ -13,8 +15,17 @@ def gig_detail(request, id):
 		return redirect('/')
 	return render(request, 'gig_detail.html', {"gig": gig})
 
+@login_required(login_url='/')
 def create_gig(request):
-	return render(request, 'create_gig.html', {})
+	if request.method == 'POST':
+		gig_form = GigForm(request.POST, request.FILES)
+		print(gig_form.is_valid())
+		print(gig_form.errors())
 
+	gig_form = GigForm()
+	return render(request, 'create_gig.html', {'gig_form': gig_form})
+
+@login_required(login_url='/')
 def my_gigs(request):
-	return render(request, 'my_gigs.html', {})
+	gigs = Gig.objects.filter(user=request.user)
+	return render(request, 'my_gigs.html', {'gigs': gigs})
